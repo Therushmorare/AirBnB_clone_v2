c3fe7c9935e4175add7cb275c665affc7c0116dc
Therushmorare <thesgo.tt.morare@gmail.com>
2023-11-23 18:49:55 -0800

    error fixes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,18 +1,5 @@
---   Creates a MySQL database
---   By name hbnb_dev_db.
---   User hbnb_dev with password hbnb_dev_pwd in localhost
-
--- Create the database if it doesn't exist
+-- Prepares a MySQL db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant all privileges on hbnb_dev_db to hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant SELECT privilege on performance_schema to hbnb_dev
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
