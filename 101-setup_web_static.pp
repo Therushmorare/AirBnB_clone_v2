5d0462a0c137645afdbdf9071dabb2aec6580b8b
Therushmorare <thesgo.tt.morare@gmail.com>
2023-12-07 13:43:13 -0800

    code fix

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,14 +1,20 @@
-# Update and upgrade the system
-exec { 'apt-update-upgrade':
-  command => '/usr/bin/apt-get update && /usr/bin/apt-get -y upgrade',
-  path    => '/usr/bin',
-}
+# web_setup.pp
 
+# Install Nginx
 package { 'nginx':
   ensure => 'installed',
 }
 
-file { '/data/web_static/releases/test':
+# Create directory structure
+file { '/data':
+  ensure => 'directory',
+}
+
+file { '/data/web_static':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/releases':
   ensure => 'directory',
 }
 
@@ -16,21 +22,32 @@ file { '/data/web_static/shared':
   ensure => 'directory',
 }
 
+file { '/data/web_static/releases/test':
+  ensure => 'directory',
+}
+
+# Create a test index.html file
 file { '/data/web_static/releases/test/index.html':
   content => 'This is a test',
 }
 
+# Create a symbolic link to set the current version
 file { '/data/web_static/current':
   ensure  => 'link',
   target  => '/data/web_static/releases/test/',
   require => File['/data/web_static/releases/test'],
+  before  => File['/etc/nginx/sites-available/default'],
 }
 
-exec { 'set-ownership':
-  command => '/bin/chown -hR ubuntu:ubuntu /data/',
-  path    => '/bin',
+# Set ownership
+file { '/data':
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  recurse => true,
 }
 
+# Add Nginx configuration for hbnb_static
 file_line { 'nginx-hbnb-static':
   path    => '/etc/nginx/sites-available/default',
   line    => "location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}",
@@ -40,9 +57,9 @@ file_line { 'nginx-hbnb-static':
   notify  => Service['nginx'],
 }
 
-# Start Nginx service
+# Restart Nginx service
 service { 'nginx':
   ensure  => 'running',
   enable  => true,
-  require => Package['nginx'],
+  require => [Package['nginx'], File['/etc/nginx/sites-available/default']],
 }
