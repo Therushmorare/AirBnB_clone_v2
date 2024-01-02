1069d9fe5bbad1caa4bf2043bddb0a6cd36588a5
therushmorare <Tshego.tt.morare@gmail.com>
2024-01-01 16:44:47 -0800

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-""" task 7 """
+"""List all states Module"""
 
-from flask import Flask
-from flask import render_template
-
+from models import storage
+from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-#@app.route()
+@app.route('/states_list')
+def states_list():
+    """display a HTML the States"""
+    all_states = list(storage.all(State).values())
+    return (render_template('7-states_list.html', all_states=all_states))
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """function that call close methofd"""
+    storage.close()
 
 
 if __name__ == '__main__':
